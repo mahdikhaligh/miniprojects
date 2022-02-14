88a6b3474077e1a816c369d8eb49086deb31a265
mahdi khaligh <94879861+mahdikhaligh@users.noreply.github.com>
2022-02-14 17:25:32 +0330

    edit

--- queue.py
+++ queue.py
@@ -1,5 +1,4 @@
-######
-# Q-3
+
 class Queue:
     """
     init -> create  list (self.items == [])
@@ -34,36 +33,13 @@ class Queue:
 def main():
     queue = Queue()
     while True:
-        item = input(" pls enter your number and you for end  enter off \n (number or off):")
+        item = input(" pls enter your number and you for exit enter (off) \n (number or off) ->")
         if item == 'off':
             break
-        queue.additem( int(item))      
+        queue.additem(int(item))      
     items = queue.show()
     print(items)    
     
 if __name__ == "__main__":
     main()
     
-########
-# Q-1
-# 8,7,6,5,4,3,2,1 
-#  ---->
-# | 8 |
-# | 7 |
-# | 6 |
-# | 5 |      -> stack 
-# | 4 |
-# | 3 |
-# | 2 | 
-# | 1 | 
-#############
-#Q-2
-# isFull -> if top1[i] == top2[i]:
-#                   return 1
-#   yani zamani ke index khone top1,top2 be yek khone dar array eshare konanad array isfull hast 
-#
-#isempty -> if top1[i] <= 0 or top2[i] <= maxsize-1:
-#                   return 1
-# zamani ke top1[i] <= 0 bashe and baraye top2[i] <= maxsize-1 bashe mishe goft array isempty hast 
-#
-
